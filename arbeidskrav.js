235f472eeadbd97b0c9e9b20ecb840129eee30ba
johaker <akerfeldtjohan@gmail.com>
2020-03-04 14:15:37 +0100

    Endret JS

--- arbeidskrav.js
+++ arbeidskrav.js
@@ -9,9 +9,9 @@ function renderTaskManager(){
     listOfTask.innerHTML = "";
     for (const task of taskList) {
         const taskDiv = document.createElement("div");
-        const { name, description } = task;
-        taskDiv.innerHTML = `<h4>${name}</h4> 
-            <div>${ description }</div> `; 
+        const { teamMember, taskDescription } = task;
+        taskDiv.innerHTML = `<h4>${ teamMember }</h4> 
+                            <div>${ taskDescription }</div> `; 
         listOfTask.appendChild(taskDiv);
     }
 }
@@ -19,24 +19,23 @@ function renderTaskManager(){
 function createNewProduct(event){
     event.preventDefault();
 
-    const name = document.querySelector("[name='name']").value;
-    const price = document.querySelector("[name='price']").value;
-    const description = document.querySelector("[name='description']").value;
+    const teamMember = document.querySelector("[name='teamMember']").value;
+    const taskDescription = document.querySelector("[name='taskDescription']").value;
 
-    const task = {name,price,description};
+    const task = {teamMember,taskDescription};
 
 
     const taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
     taskList.push(task);
-    window.localStorage.setItem("taskList", JSON.stringify(productList));
-    renderProductList();
+    window.localStorage.setItem("taskList", JSON.stringify(taskList));
+    renderTaskManager();
 
     event.target.reset();
 }
 renderTaskManager();
 
 window.addEventListener("storage", function(event) {
-    if (event.key === "taskListList") {
-        renderProductList();
+    if (event.key === "taskList") {
+        renderTaskManager();
     }
 });
